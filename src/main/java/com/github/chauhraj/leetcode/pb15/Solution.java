d16ad8ff102192df28d296613f0f3e9e826affa3
Rajiv Chauhan <rajiv.chauhan@gmail.com>
2016-05-22 12:50:37 -0400

    Committing the accepted solution for 3Sum Problem.

--- src/main/java/com/github/chauhraj/leetcode/pb15/Solution.java
+++ src/main/java/com/github/chauhraj/leetcode/pb15/Solution.java
@@ -1,6 +1,4 @@
-
 package com.github.chauhraj.leetcode.pb15;
-
 import static java.util.Arrays.*;
 
 import java.util.Collections;
@@ -11,72 +9,124 @@ public class Solution {
     public List<List<Integer>> threeSum(int[] input) {
         if(input.length < 3) {
             return Collections.emptyList();
+        } else if(input.length == 3) {
+            int sum = input[0] + input[1] + input[2];
+            if(sum == 0) {
+                sort(input);
+                List<List<Integer>> result = new LinkedList<>();
+                List<Integer> l = new LinkedList<>();
+                l.add(input[0]);
+                l.add(input[1]);
+                l.add(input[2]);
+                result.add(l);
+                return result;
+            } else {
+                return Collections.emptyList();
+            }
+        } else {
+            List<List<Integer>> results = new LinkedList<>();
+            sort(input);
+            threeSum(input, results);
+            return results;
         }
-        List<List<Integer>> results = new LinkedList<>();
-        sort(input);
-        threeSum(input, results);
-        return results;
     }
 
     private void threeSum(int[] A, List<List<Integer>> results) {
-        int nonNegIdx = findFirstNonNegIdx(A);
-        if(nonNegIdx == 0) {
-            // there are no negative numbers. So the rest of the numbers are >= 0
-            // So only possible solution is when a[0] = a[1] = a[2] = 0
-            if(A[0] == 0 && A[0] == A[1] && A[1] == A[2]) {
-                results.add(l(0, 0, 0));
-                return;
-            }
+        int nonNegIdx = binarySearch0(A, 0, A.length - 1, 0);
+        if(nonNegIdx >= A.length) {
+            return ;
         }
-        for(int i = nonNegIdx; i < A.length; i++) {
-            for(int j = nonNegIdx - 1; j >= 0; j--) {
-                int sum = (A[i] + A[j]);
-                if(sum < 0) {
-                    //+# < -# for eg => (2, -4)
-                    if(-sum > A[i]) {
-                        int idx = binarySearch(A, i + 1, A.length, -sum);
-                        if (idx >= 0) {
-                            // found the solution....
-                            results.add(l(A[j], A[i], A[idx]));
-                        }
-                    } else if(-sum == A[i] && i != A.length - 1) {
-                        results.add(l(A[j], A[i], A[i]));
-                        break;
-                    }
+        int negRegionBoundIdx = nonNegIdx, posRegionBoundaryIdx = nonNegIdx;
+        if(A[nonNegIdx] == 0) {
+            while(posRegionBoundaryIdx < A.length && A[posRegionBoundaryIdx] == 0) posRegionBoundaryIdx++; posRegionBoundaryIdx--;
+            while(negRegionBoundIdx >= 0 && A[negRegionBoundIdx] == 0) negRegionBoundIdx--; negRegionBoundIdx++;
+        }
+        findSolutionsOnNegativePartition(A, negRegionBoundIdx, results);
+        if((posRegionBoundaryIdx - negRegionBoundIdx + 1) >= 3) {
+            List<Integer> l = new LinkedList<>();
+            l.add(0);
+            l.add(0);
+            l.add(0);
+            results.add(l);
+        }
+        findSolutionsOnPositivePartition(A, posRegionBoundaryIdx, results);
+    }
+
+    private void findSolutionsOnNegativePartition(int[] A, int boundaryIdx, List<List<Integer>> results) {
+        if(A[0] >= 0) {
+            return;
+        }
+        int solnSpcStartIdx = boundaryIdx,
+                solnSpcEndIdx = A.length - 1;
+        for(int i = 0; i < boundaryIdx; i++) {
+            while(i < boundaryIdx - 1 && A[i+1] == A[i]) i++;
+            int current = A[i],
+                    keyIndex = binarySearch0(A, solnSpcStartIdx, solnSpcEndIdx, -current),
+                    firstIndex = solnSpcStartIdx,
+                    lastIndex = Integer.min(keyIndex, A.length - 1);
+            solnSpcEndIdx = lastIndex;
+            while(firstIndex < lastIndex) {
+                int sum = A[firstIndex] + A[lastIndex] + current;
+                if(sum == 0) {
+                    List<Integer> l = new LinkedList<>();
+                    l.add(current);
+                    l.add(A[firstIndex]);
+                    l.add(A[lastIndex]);
+                    results.add(l);
+                    while(firstIndex < lastIndex - 1 && A[firstIndex] == A[firstIndex + 1]) firstIndex++;
+                    firstIndex++;
+                } else if(sum > 0) {
+                    while(lastIndex > firstIndex + 1 && A[lastIndex] == A[lastIndex - 1]) lastIndex--;
+                    lastIndex--;
                 } else {
-                    //+# > -# for eg => (2, -1)
-                    if(-sum < A[j]) {
-                        int idx = binarySearch(A, 0, j, -sum);
-                        if (idx >= 0) {
-                            // found the solution....
-                            results.add(l(A[idx], A[j], A[i]));
-                            break;
-                        }
-                    }else if(-sum == A[j] && j != 0) {
-                        results.add(l(A[j], A[j], A[i]));
-                        break;
-                    }
-
+                    while(firstIndex < lastIndex - 1 && A[firstIndex] == A[firstIndex + 1]) firstIndex++;
+                    firstIndex++;
                 }
-                while(j > 0 && A[j] == A[j - 1]) j--;
             }
-            while(i < A.length - 1 && A[i] == A[i + 1]) i--;
+        }
+    }
+    private void findSolutionsOnPositivePartition(int[] A, int boundaryIdx, List<List<Integer>> results) {
+        int solnSpcStartIdx = 0,
+                solnSpcEndIdx = boundaryIdx;
+        for(int i = A.length - 1; i >= boundaryIdx; i--) {
+            while(i > boundaryIdx - 1 && A[i] == A[i - 1]) i--;
+            int current = A[i];
+            int keyIndex = binarySearch0(A, solnSpcStartIdx, solnSpcEndIdx, -current),
+                    lastIndex = boundaryIdx - 1,
+                    firstIndex = keyIndex;
+            solnSpcStartIdx = keyIndex;
+            if(A[firstIndex] == -current) {
+                while(firstIndex < boundaryIdx - 1 && A[firstIndex] == A[firstIndex + 1]) firstIndex++;
+                firstIndex++;
+            }
+            while(firstIndex < lastIndex) {
+                int sum = A[firstIndex] + A[lastIndex] + current;
+                if(sum == 0) {
+                    List<Integer> l = new LinkedList<>();
+                    l.add(A[firstIndex]);
+                    l.add(A[lastIndex]);
+                    l.add(current);
+                    results.add(l);
+                    while(lastIndex > firstIndex + 1 && A[lastIndex] == A[lastIndex - 1]) lastIndex--;
+                    lastIndex--;
+                } else if(sum > 0) {
+                    while(lastIndex > firstIndex + 1 && A[lastIndex] == A[lastIndex - 1]) lastIndex--;
+                    lastIndex--;
+                } else {
+                    while(firstIndex < lastIndex - 1 && A[firstIndex] == A[firstIndex + 1]) firstIndex++;
+                    firstIndex++;
+                }
+            }
         }
     }
 
-    private List<Integer> l(Integer... az) {
-        return asList(az);
+    private int binarySearch0(int[] A, int solnSpcStartIdx, int solnSpcEndIdx, int key) {
+        int index = binarySearch(A, solnSpcStartIdx, solnSpcEndIdx + 1, key);
+        if(index < 0) {
+            index += 1;
+            index = -index;
+        }
+        return index;
     }
 
-    private int findFirstNonNegIdx(int[] A) {
-        int nonNegIdx = binarySearch(A, 0);
-        if(nonNegIdx < 0) {
-            // Didn't find 0 and it doesn't matter because then it will return the index of the lowest +ve number
-            nonNegIdx = -(nonNegIdx + 1);
-        } else {
-            while(nonNegIdx > 0 && A[nonNegIdx] == A[nonNegIdx - 1]) nonNegIdx--;
-        }
-        return nonNegIdx;
-    }
 }
-        
--- src/test/java/com/github/chauhraj/leetcode/pb15/SolutionTest.java
+++ src/test/java/com/github/chauhraj/leetcode/pb15/SolutionTest.java
@@ -3,6 +3,7 @@ package com.github.chauhraj.leetcode.pb15;
 
 import static org.hamcrest.CoreMatchers.*;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -23,19 +24,55 @@ public class SolutionTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {a(-1, 2, 1), ll(l(), l())},
-                {a(0, 0), ll(l())},
+                {a(-2,-4,-5,-5,-2,-3,-5,-4,-2), ll()},
+                {a(-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0), ll(l(-5,1,4),l(-4,0,4),l(-4,1,3),l(-2,-2,4),l(-2,1,1),l(0,0,0))},
+                {a(-2,0,0,2,2), ll(l(-2, 0, 2))},
+                {a(0, 0, 0, 0), ll(l(0, 0, 0))},
                 {a(0, 0, 0), ll(l(0, 0, 0))},
+                {a(-1, 0, 1, 2, 0, -1, -4, 0), ll(l(-1, 0, 1), l(-1, -1, 2), l(0, 0, 0))},
+                {a(-1, 0, 1, 2, -1, -4), ll(l(-1, 0, 1), l(-1, -1, 2))},
+                {a(5, 4, -3, -4, -2, 0, -1, -1, 2), ll(l(-4,-1,5),l(-4,0,4),l(-3,-2,5),l(-3,-1,4),l(-2,0,2),l(-1,-1,2))},
+                {a(-1, 2, 1), ll()},
+                {a(0, 0), ll()},
                 {a(-1, 2, -1), ll(l(-1, -1, 2))},
                 {a(-1, 1, 2, -1, -4), ll(l(-1, -1, 2))},
-                {a(-1, 0, 1, 2, -1, -4), ll(l(-1, 0, 1), l(-1, -1, 2))}
         });
     }
 
     @Test
     public void executethreeSumTest() {
         Solution solution = new Solution();
-        assertThat(solution.threeSum(input), is(expected));
+        List<List<Integer>> result = solution.threeSum(input);
+        assertThat(result.size(), is(expected.size()));
+        Collections.sort(expected, SolutionTest::comparator);
+        Collections.sort(result, SolutionTest::comparator);
+        for(int i = 0; i < result.size(); i++) {
+            assertTrue(CollectionUtils.isEqualCollection(result.get(i), expected.get(i)));
+        }
+    }
+
+    private static int comparator(List<Integer> l1, List<Integer> l2) {
+        int x1 = l1.get(0), y1 = l1.get(1), z1 = l1.get(2);
+        int x2 = l2.get(0), y2 = l2.get(1), z2 = l2.get(2);
+        if(x1 < x2) {
+            return -1;
+        } else if(x1 == x2) {
+            if(y1 < y2) {
+                return -1;
+            } else if(y1 == y2) {
+                if(z1 < z2) {
+                    return -1;
+                } else if(z1 == z2) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            } else {
+                return 1;
+            }
+        } else {
+            return 1;
+        }
     }
 
     private static List<List<Integer>> ll(List<Integer>... al) {
