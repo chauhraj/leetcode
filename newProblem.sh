a98581fccf06d9a3622f0a1545d93c792925c9fe
Rajiv Chauhan <rajiv.chauhan@gmail.com>
2016-06-04 00:12:55 -0400

    pb15-3sum

--- newProblem.sh
+++ newProblem.sh
@@ -9,4 +9,11 @@ if [[ $# -ne 4 ]]; then
     usage ;
 fi
 
+BASE_URL="https://leetcode.com"
+BRANCH=$(git symbolic-ref --short HEAD)
+PROBLEM_NAME=$(wget -qO- $BASE_URL/problemset/algorithms/ | grep -A 3 "<td>${1}</td>" | sed -n "s/.*href=['\"]\([^'\"]*\)['\"].*/\1/gp" | awk -F / '{print $3}')
+if [[ "$BRANCH" == "master" ]]; then
+    git checkout -b "pb${1}/${PROBLEM_NAME}"
+fi
+MAIN_PROBLEM="${BASE_URL}/problems/$PROBLEM_NAME"
 gradle -q newProblem -PleetId=$1 -Pmethod=$2 -PinputType=$3 -PoutputType=$4
---
+++ src/main/java/com/github/chauhraj/Utils.java
@@ -0,0 +1,22 @@
+package com.github.chauhraj;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by chauhraj on 5/30/16.
+ */
+public class Utils {
+    public static List<List<Integer>> ll(List<Integer>... al) {
+        return Arrays.asList(al);
+    }
+
+    public static List<Integer> l(Integer... nos) {
+        return Arrays.asList(nos);
+    }
+
+    public static int[] a(int... nos) {
+        return nos;
+    }
+
+}
